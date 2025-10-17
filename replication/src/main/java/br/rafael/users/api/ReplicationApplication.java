77184132f15691dba1034e18e1b8a5237c87029c
S1mp3r <rafaelcarneiro19@edu.unifor.br>
2025-10-16 21:30:32 -0300

    feat(Replication Microservice): setting up the replication microservice and mongodb configurations

---
+++ replication/src/main/java/br/rafael/users/api/ReplicationApplication.java
@@ -0,0 +1,13 @@
+package br.rafael.users.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ReplicationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ReplicationApplication.class, args);
+	}
+
+}
